11384496783781c4c49888f2ee2572ef1e2b5c04
Nicola Gruhl <ngruhl@welfenlab.de>
2015-12-16 16:05:25 +0100

    (update) dynatype-network graph is now working and compatible to the other graph

--- src/dynatype-network.js
+++ src/dynatype-network.js
@@ -1,84 +1,72 @@
-/*
-normal node:
-id = name of node
-label = motherNode
-
-input-node:
-id = id of node + '_IN_' + number
-label = input datatype
-
-output-node:
-id = id of node + '_OUT_' + number
-label = output datatype
-
-translator-node:
-id = id of node 1 + ':' + id of node 2 + '_' + number
-label = type of node 1 + ':' + type of node 2
-
-translator-input-node:
-id = id of translator-node + '_IN_' + number
-label = translator_in
-
-translator-output-node:
-id = id of translator-node + '_OUT_' + number
-label = translator_out
-
-edge from output- to input-node:
-label = 'edge'
-
-edge from input- to mother-node:
-label = 'motherEdge_in'
-
-edge from mother- to output-node:
-label = 'motherEdge_out'
-
-edge from input- to translator-node:
-label = 'converterEdge_in'
-
-edge from translator- to output-node:
-label = 'converterEdge_out'
-*/
-
 export function addTypeConverting (processGraph, convertGraph) {
+  var fs = require('fs')
+  var compLib = '../component-library/meta/'
   var grlib = require('graphlib')
-  var newProcessGraph = new grlib.Graph({ directed: true, compound: false, multigraph: true })
-  var nodes = processGraph.nodes()
-  for (var i = 0; i < processGraph.nodeCount(); i++) {
-    newProcessGraph.setNode(nodes[i], processGraph.node(nodes[i]))
-  }
+  var newProcessGraph = processGraph
   var edges = processGraph.edges()
-  for (var l = 0; l < processGraph.edgeCount(); l++) {
-    newProcessGraph.setEdge(edges[l].v, edges[l].w, processGraph.edge(edges[l]))
-  }
-  for (var j = 0; j < processGraph.edgeCount(); j++) {
-    if (processGraph.node(edges[j].v) !== 'motherNode' && processGraph.node(edges[j].w) !== 'motherNode' && processGraph.node(edges[j].v) !== 'translator_in' && processGraph.node(edges[j].w) !== 'translator_in' && processGraph.node(edges[j].v) !== 'translator_out' && processGraph.node(edges[j].w) !== 'translator_out') {
-      newProcessGraph.removeEdge(edges[j].v, edges[j].w)
-      var typeV = processGraph.node(edges[j].v)
-      var typeW = processGraph.node(edges[j].w)
-      var dijkstra = grlib.alg.dijkstra(convertGraph, typeV)
-      var way = [typeW]
-      while (way[way.length - 1] !== typeV) {
-        way.push(dijkstra[way[way.length - 1]].predecessor)
+  // Add Translator nodes
+  var edgeCount = processGraph.edgeCount()
+  for (var j = 0; j < edgeCount; j++) {
+    // Translator nodes only exist between Port
+    if ((processGraph.node(edges[j].v)['nodeType'] === 'inPort' || processGraph.node(edges[j].v)['nodeType'] === 'outPort') && (processGraph.node(edges[j].w)['nodeType'] === 'inPort' || processGraph.node(edges[j].w)['nodeType'] === 'outPort')) {
+      // get datatypes
+      var processV = edges[j].v.substring(0, edges[j].v.indexOf('_', 2))
+      var processW = edges[j].w.substring(0, edges[j].w.indexOf('_', 2))
+      var portNameV = edges[j].v.substring(edges[j].v.lastIndexOf('_') + 1)
+      var portNameW = edges[j].w.substring(edges[j].w.lastIndexOf('_') + 1)
+      var metaV = newProcessGraph.node(processV)['meta']
+      var metaW = newProcessGraph.node(processW)['meta']
+      var typeV
+      var typeW
+      if (processGraph.node(edges[j].v)['nodeType'] === 'inPort') {
+        typeV = JSON.parse(fs.readFileSync(compLib + metaV + '.json'))['inputPorts'][portNameV]
+      } else {
+        typeV = JSON.parse(fs.readFileSync(compLib + metaV + '.json'))['outputPorts'][portNameV]
       }
-      for (var k = way.length - 1; k >= 1; k--) {
-        var number = way.length - k
-        var id = edges[j].v + ':' + edges[j].w + '_' + number
-        newProcessGraph.setNode(id, way[k] + ':' + way[k - 1])
-        newProcessGraph.setNode(id + '_IN_1', 'translator_in')
-        newProcessGraph.setNode(id + '_OUT_1', 'translator_out')
-        newProcessGraph.setEdge(id + '_IN_1', id, 'converterEdge_in')
-        newProcessGraph.setEdge(id, id + '_OUT_1', 'converterEdge_out')
-        if (k === way.length - 1) {
-          newProcessGraph.setEdge(edges[j].v, id + '_IN_1', 'edge')
-        } else {
-          newProcessGraph.setEdge(edges[j].v + ':' + edges[j].w + '_' + (number - 1) + '_OUT_1', id + '_IN_1', 'edge')
+      if (processGraph.node(edges[j].w)['nodeType'] === 'inPort') {
+        typeW = JSON.parse(fs.readFileSync(compLib + metaW + '.json'))['inputPorts'][portNameW]
+      } else {
+        typeW = JSON.parse(fs.readFileSync(compLib + metaW + '.json'))['outputPorts'][portNameW]
+      }
+      // if the types are different add translator
+      if (typeV !== typeW) {
+        newProcessGraph.removeEdge(edges[j].v, edges[j].w)
+        var parentV = processGraph.parent(edges[j].v)
+        // datatype translator
+        var dijkstra = grlib.alg.dijkstra(convertGraph, typeV)
+        var way = [typeW]
+        while (way[way.length - 1] !== typeV) {
+          way.push(dijkstra[way[way.length - 1]].predecessor)
         }
-        if (k === 1) {
-          newProcessGraph.setEdge(id + '_OUT_1', edges[j].w, 'edge')
+        for (var k = way.length - 1; k >= 1; k--) {
+          var number = way.length - k
+          var id = edges[j].v + ':' + edges[j].w + '_' + number
+          // translator nodes
+          newProcessGraph.setNode(id, {'nodeType': 'translator', 'typeFrom': way[k], 'typeTo': way[k - 1], 'parent': parentV})
+          newProcessGraph.setNode(id + '_PORT_in', {'nodeType': 'inPort_trans', 'portName': 'in'})
+          newProcessGraph.setNode(id + '_PORT_out', {'nodeType': 'outPort_trans', 'portName': 'out'})
+          // translator edges
+          newProcessGraph.setEdge(id + '_PORT_in', id)
+          newProcessGraph.setEdge(id, id + '_PORT_out')
+          // set parents  TODO: parent ok?
+          newProcessGraph.setParent(id, parentV)
+          newProcessGraph.setParent(id + '_PORT_in', parentV)
+          newProcessGraph.setParent(id + '_PORT_out', parentV)
+          // edges between translators and between processes and translators
+          // is first translator
+          if (k === way.length - 1) {
+            newProcessGraph.setEdge(edges[j].v, id + '_PORT_in')
+          } else {
+            newProcessGraph.setEdge(edges[j].v + ':' + edges[j].w + '_' + (number - 1) + '_PORT_out', id + '_PORT_in')
+          }
+          // is last translator
+          if (k === 1) {
+            newProcessGraph.setEdge(id + '_PORT_out', edges[j].w)
+          }
         }
       }
     }
   }
-  console.log(JSON.stringify(newProcessGraph, null, 2))
+  // fs.writeFileSync('test/fixtures/testgraph.graphlib', JSON.stringify(grlib.json.write(newProcessGraph), null, 2))
   return newProcessGraph
 }
--- test/dynatype-network.js
+++ test/dynatype-network.js
@@ -3,9 +3,10 @@
 var expect = require('chai').expect
 var dtypenet = require('../src/dynatype-network.js')
 var grlib = require('graphlib')
+var fs = require('fs')
 
 var convertGraph = new grlib.Graph({ directed: true, compound: false, multigraph: false })
-var processGraph = new grlib.Graph({ directed: true, compound: true, multigraph: true })
+var processGraph = grlib.json.read(JSON.parse(fs.readFileSync('../npg-port-remodeler/test/fixtures/testgraph.graphlib')))
 
 convertGraph.setNode('int', 'int')
 convertGraph.setNode('float', 'float')
@@ -15,88 +16,12 @@ convertGraph.setEdge('int', 'float', 'int_float')
 convertGraph.setEdge('float', 'string', 'float_string')
 convertGraph.setEdge('string', 'int', 'string_int')
 
-// process Nodes
-processGraph.setNode('0_STDIN', {nodeType: 'process', type: 'io/stdin'})
-processGraph.setNode('1_INC', {nodeType: 'process', type: 'math/inc'})
-processGraph.setNode('2_STDOUT', {nodeType: 'process', type: 'io/stdout'})
-// port Nodes
-processGraph.setNode('0_STDIN_OUTPORT_output', {nodeType: 'outPort', portName: 'output'})
-processGraph.setNode('1_INC_INPORT_i', {nodeType: 'inPort', portName: 'i'})
-processGraph.setNode('1_INC_OUTPORT_inc', {nodeType: 'outPort', portName: 'inc'})
-processGraph.setNode('2_STDOUT_INPORT_input', {nodeType: 'inPort', portName: 'input'})
-
-// edges from/to ports
-processGraph.setEdge('0_STDIN', '0_STDIN_OUTPORT_output')
-processGraph.setEdge('1_INC_INPORT_i', '1_INC')
-processGraph.setEdge('1_INC', '1_INC_OUTPORT_inc')
-processGraph.setEdge('2_STDOUT_INPORT_input', '2_STDOUT')
-
-// edges between ports
-processGraph.setEdge('0_STDIN_OUTPORT_output', '1_INC_INPORT_i')
-processGraph.setEdge('1_INC_OUTPORT_inc', '2_STDOUT_INPORT_input')
-
 describe('Dynamic type network graph', function () {
-  it('Creates a processgraph with the same nodes', function () {
+  it('Creates a processgraph with correct translator nodes', function () {
     var d = dtypenet.addTypeConverting(processGraph, convertGraph)
-    expect(d).to.be.ok
-    expect(d.node('0_STDIN')['nodeType']).to.be.equal('process')
-    expect(d.node('0_STDIN')['type']).to.be.equal('io/stdin')
-    expect(d.node('1_INC')['nodeType']).to.be.equal('process')
-    expect(d.node('1_INC')['type']).to.be.equal('math/inc')
-    expect(d.node('2_STDOUT')['nodeType']).to.be.equal('process')
-    expect(d.node('2_STDOUT')['type']).to.be.equal('io/stdout')
-
-    expect(d.node('0_STDIN_OUTPORT_output')['nodeType']).to.be.equal('outPort')
-    expect(d.node('0_STDIN_OUTPORT_output')['portName']).to.be.equal('output')
-    expect(d.node('1_INC_INPORT_i')['nodeType']).to.be.equal('inPort')
-    expect(d.node('1_INC_INPORT_i')['portName']).to.be.equal('i')
-    expect(d.node('1_INC_OUTPORT_inc')['nodeType']).to.be.equal('outPort')
-    expect(d.node('1_INC_OUTPORT_inc')['portName']).to.be.equal('inc')
-    expect(d.node('2_STDOUT_INPORT_input')['nodeType']).to.be.equal('inPort')
-    expect(d.node('2_STDOUT_INPORT_input')['portName']).to.be.equal('input')
-  })
-
-  it('Creates a processgraph with type-convertion', function () {
-    var d = dtypenet.addTypeConverting(processGraph, convertGraph)
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0')['nodeType']).to.be.equal('translator')
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0')['typeFrom']).to.be.equal('string')
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0')['typeTo']).to.be.equal('int')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0')['nodeType']).to.be.equal('translator')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0')['typeFrom']).to.be.equal('int')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0')['typeTo']).to.be.equal('float')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1')['nodeType']).to.be.equal('translator')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1')['typeFrom']).to.be.equal('float')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1')['typeTo']).to.be.equal('string')
-
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_INPORT_in')['nodeType']).to.be.equal('inPort')
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_INPORT_in')['portName']).to.be.equal('in')
-
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_OUTPORT_out')['nodeType']).to.be.equal('outPort')
-    expect(d.node('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_OUTPORT_out')['portName']).to.be.equal('out')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0_INPORT_in')['nodeType']).to.be.equal('inPort')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0_INPORT_in')['portName']).to.be.equal('in')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0_OUTPORT_out')['nodeType']).to.be.equal('outPort')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_0_OUTPORT_out')['portName']).to.be.equal('out')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1_INPORT_in')['nodeType']).to.be.equal('inPort')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1_INPORT_in')['portName']).to.be.equal('in')
-
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1_OUTPORT_out')['nodeType']).to.be.equal('outPort')
-    expect(d.node('1_INC_OUTPORT_inc:2_STDOUT_INPORT_input_1_OUTPORT_out')['portName']).to.be.equal('out')
-  })
-  it('All nodes are connected with edges', function () {
-    var d = dtypenet.addTypeConverting(processGraph, convertGraph)
-    expect(d.edge('0_STDIN', '0_STDIN_OUTPORT_output')).to.be.ok
-    expect(d.edge('0_STDIN_OUTPORT_output', '0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_INPORT_in')).to.be.ok
-    expect(d.edge('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_INPORT_in', '0_STDIN_OUTPORT_output:1_INC_INPORT_i_0')).to.be.ok
-    expect(d.edge('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0', '0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_OUTPORT_out')).to.be.ok
-    expect(d.edge('0_STDIN_OUTPORT_output:1_INC_INPORT_i_0_OUTPORT_out', '1_INC_INPORT_i')).to.be.ok
-    expect(d.edge('1_INC_INPORT_i', '1_INC')).to.be.ok
-    expect(d.edge('1_INC', '1_INC_OUTPORT_inc')).to.be.ok
+    var curGraph = grlib.json.write(d)
+    var testgraph = JSON.parse(fs.readFileSync('test/fixtures/testgraph.graphlib'))
+    expect(curGraph).to.deep.equal(testgraph)
   })
   it('The graph with added translators should not be changed', function () {
     var d = dtypenet.addTypeConverting(processGraph, convertGraph)
